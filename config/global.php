201021e5481c37c78f70387101e4ccc0005bcc80
Elisa Villa <202160467@ucc.mx>
2024-05-17 11:04:43 -0600

    Pantalla Modificada

--- config/global.php
+++ config/global.php
@@ -51,7 +51,7 @@ EOD;
 
 function getNavbar($ruta = ''){
     $html = <<<EOD
-<nav class="navbar navbar-expand navbar-dark custom-bg static-top">
+<nav class="navbar navbar-expand navbar-dark  custom-bg static-top">
 
     <a class="navbar-brand mr-1" href="{$ruta}index.php">UCC</a>
 
