43e97af41201be89f2ee53a588d3e16d6ef08646
Elisa Villa <202160467@ucc.mx>
2024-05-16 18:42:24 -0600

    Pantalla Academia

---
+++ app/admin/catalogos/academia/usuarios_academia.php
@@ -0,0 +1,114 @@
+<?php
+require_once '../../../../config/global.php';
+
+define('RUTA_INCLUDE', '../../../../'); //ajustar a necesidad
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo PAGE_TITLE ?></title>
+
+    <?php getTopIncludes(RUTA_INCLUDE ) ?>
+</head>
+
+<body id="page-top">
+
+<?php getNavbar() ?>
+
+<div id="wrapper">
+
+    <?php getSidebar() ?>
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">Catálogos</li>
+                    <li class="breadcrumb-item active" aria-current="page">Usuario Academia</li>
+                </ol>
+            </nav>
+
+          <!-- <div class="alert alert-success" role="alert">
+                <i class="fas fa-check"></i> Mensaje de éxito
+            </div>
+
+            <div class="alert alert-danger" role="alert">
+                <i class="fas fa-exclamation-triangle"></i> Mensaje de error
+            </div>
+            -->
+            <div class="row my-3">
+                <div class="col text-right">
+                    <button type="button" class="btn btn-primary"><i class="fas fa-plus"></i> Agregar Usuario</button>
+                </div>
+            </div>
+
+            <div class="table-responsive mb-3">
+                <table class="table table-bordered dataTable">
+                    <thead>
+                    <tr>
+                        <th>Nombre Completo</th>
+                        <th>Correo Electrónico</th>
+                        <th>Número de Teléfono</th>
+                        <th>Puesto</th>
+                        <th>Acciones</th>
+                    </tr>
+                    </thead>
+                    <tfoot>
+                    <th>Nombre Completo</th>
+                    <th>Correo Electrónico</th>
+                    <th>Número de Teléfono</th>
+                    <th>Puesto</th>
+                    <th>Acciones</th>
+                    </tfoot>
+                    <tbody>
+                    <tr>
+                        <td>Maria del Carmen Torres Aguirre</td>
+                        <td>202851268@ucc.mx</td>
+                        <td>123-456-7891</td>
+                        <td>Aseguramiento de Calidad</td>
+                        <td><a href="#" class="btn btn-link btn-sm btn-sm">Editar</a> <a href="#" class="btn btn-link btn-sm">Eliminar</a></td>
+                    </tr>
+
+                    </tbody>
+                    <tr>
+                        <td>Elisa Villa</td>
+                        <td>202160467@ucc.mx</td>
+                        <td>123-456-7891</td>
+                        <td>Alumno</td>
+                        <td><a href="#" class="btn btn-link btn-sm btn-sm">Editar</a> <a href="#" class="btn btn-link btn-sm">Eliminar</a></td>
+                    </tr>
+
+                </table>
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php getFooter() ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<?php getModalLogout() ?>
+
+<?php getBottomIncudes( RUTA_INCLUDE ) ?>
+</body>
+
+</html>
