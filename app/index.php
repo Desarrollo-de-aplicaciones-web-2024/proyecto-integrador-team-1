a3dcfca07ddaa2b95e1c044dcc4fe99f7fea115c
jose adolfo <202160541@ucc.mx>
2024-05-27 13:44:56 -0600

    pantallas act

--- app/index.php
+++ app/index.php
@@ -2,19 +2,13 @@
 <html lang="es">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>Universidad Cristóbal Colón</title>
-
-    <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template-->
     <link href="../css/sb-admin.css" rel="stylesheet">
     <link href="../img/favicon.ico" rel="shortcut icon" type="image/png"/>
     <style>
@@ -78,23 +72,20 @@
         <div class="card-header">¡Bienvenido!</div>
         <div class="card-body">
             <img src="../img/escudoo.svg" alt="Escudo de la Universidad Cristóbal Colón">
-            <form>
+            <form action="login.php" method="post">
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="email" id="inputEmail" class="form-control" placeholder="Correo electrónico"
-                               required="required" autofocus="autofocus">
+                        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Correo electrónico" required autofocus>
                         <label for="inputEmail">Correo electrónico</label>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="form-label-group">
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Contraseña"
-                               required="required">
+                        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Contraseña" required>
                         <label for="inputPassword">Contraseña</label>
                     </div>
                 </div>
-
-                <a class="btn btn-primary btn-block" href="index.php">Ingresar</a>
+                <button class="btn btn-primary btn-block" type="submit">Ingresar</button>
                 <a class="btn btn-secondary btn-block" href="registrar.php">Registro</a>
             </form>
             <div class="text-center">
@@ -107,7 +98,6 @@
 <!-- Bootstrap core JavaScript-->
 <script src="../vendor/jquery/jquery.min.js"></script>
 <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
 <!-- Core plugin JavaScript-->
 <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
