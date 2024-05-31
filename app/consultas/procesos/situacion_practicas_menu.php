ae6d4f54774906775913a04d3b31bb70eab00924
Elvis Gamboa <202260011@ucc.mx>
2024-05-31 17:58:01 -0600

    Pantallas situación prácticas

---
+++ app/consultas/procesos/situacion_practicas_menu.php
@@ -0,0 +1,108 @@
+<?php
+require_once '../../../config/global.php';
+define('RUTA_INCLUDE', '../../../'); //ajustar a necesidad
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="https://fonts.googleapis.com/css2?family=Liberation+Sans&display=swap" rel="stylesheet">
+    <!-- Bootstrap 5.1 CSS -->
+
+    <title><?php echo PAGE_TITLE ?></title>
+
+
+    <?php getTopIncludes(RUTA_INCLUDE ) ?>
+    <style>
+        .table-hover tbody tr:hover {
+            color: #007faf;
+            cursor: pointer; /* Change cursor to pointer on hover */
+        }
+
+        .my-custom-font {
+            font-family: 'Liberation Sans', sans-serif;
+        }
+
+
+    </style>
+
+</head>
+
+<body id="page-top">
+
+<?php getNavbar() ?>
+
+<div id="wrapper">
+
+    <?php getSidebar() ?>
+
+    <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Content -->
+
+            <div class="container mt-5">
+                <div class="mt-5 mb-3 text-center">
+                    <h1 class="">Situación de Prácticas Profesionales</h1>
+
+                </div>
+                <hr>
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar w/ text</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarText">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Pricing</a>
+                                </li>
+                            </ul>
+                            <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php getFooter() ?>
+
+    </div>
+    <!-- /.content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<?php getModalLogout() ?>
+
+<?php getBottomIncudes( RUTA_INCLUDE ) ?>
+
+
+</body>
+
+</html>
