62d57dd5730d4b3a4500cbbf07a9ded6ab83ed13
Elisa Villa <202160467@ucc.mx>
2024-06-03 15:03:46 -0600

    Agregar Usuario, editar usuario con ventana emergente

--- app/admin/catalogos/academia/editar_usuario.php
+++ app/admin/catalogos/academia/editar_usuario.php
@@ -3,16 +3,20 @@ require_once '../../../../config/global.php';
 require_once '../../../../config/db.php';
 
 if ($_POST) {
+    // Obtener los datos del formulario
     $id = $_POST['id'];
     $nombre_completo = $_POST['nombre_completo'];
     $correo = $_POST['correo'];
     $telefono = $_POST['telefono'];
     $cargo = $_POST['cargo'];
 
+    // Actualizar los datos en la base de datos
     $sql = "UPDATE academia_usuarios SET nombre_completo='$nombre_completo', correo='$correo', telefono='$telefono', cargo='$cargo' WHERE id=$id";
     if (mysqli_query($conexion, $sql)) {
-        echo "Usuario actualizado exitosamente.";
+        // Si se actualizan los datos correctamente, muestra un mensaje de éxito y recarga la página
+        echo "<script>alert('Cambios guardados correctamente.'); window.location.href = 'usuarios_academia.php';</script>";
     } else {
+        // Si hay un error al actualizar los datos, muestra un mensaje de error
         echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
     }
 }
