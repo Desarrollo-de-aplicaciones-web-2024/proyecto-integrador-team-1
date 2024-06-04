d81efaaa37fe2129dd4aec955ef7bf8e17084062
alan <202160187@ucc.mx>
2024-06-03 20:21:49 -0600

    modificacion a vacantes y agregada la pagina principal (inicio) del alumno. parte 4 FINAL
    YA ALFIN TENGO IMAGENES AAAAAAAAAAAAA

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'database-team1-daw.c30w0agw4764.us-east-2.rds.amazonaws.com');
 define('DB_USER', 'admin');
 define('DB_PASS', 'S1stemas_23');
-define('DB_NAME', 'sgpp');
+define('DB_NAME', 'PP_TEAM1');
 
 $conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
